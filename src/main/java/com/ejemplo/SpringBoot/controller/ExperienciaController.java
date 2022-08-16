2fd6a7148f7ebd7c3fc0c1cb0628a2673df8666a
Marcela <mavia2@gmail.com>
2022-08-16 20:50:18 -0300

    arregle autentificacion y agregue los rest controller

--- src/main/java/com/ejemplo/SpringBoot/controller/ExperienciaController.java
+++ src/main/java/com/ejemplo/SpringBoot/controller/ExperienciaController.java
@@ -12,8 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class ExperienciaController {
      @Autowired
     private IExperienciaService experienciaService;
--- src/main/java/com/ejemplo/SprintBoot/configuration/WebSecurityConfig.java
+++ src/main/java/com/ejemplo/SprintBoot/configuration/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable()
 				// Los endpoints /login y /register no necesitan ser autenticados.			
-				.authorizeRequests().antMatchers("/login","/register","/get/persona").permitAll().
+				.authorizeRequests().antMatchers("/login","/register").permitAll().
 				// El resto de los endpoints necesita el token JWT para validar el request.
 				anyRequest().authenticated().and().
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
